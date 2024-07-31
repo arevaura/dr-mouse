bc62d5a67423c3d7afb1710a1f25892709d6f8b0
Eva <125091369+eva820@users.noreply.github.com>
2024-07-31 00:16:15 -0700

    Clean code and add basic documentation to all methods.

--- src/main/ui/gui/JournalUI.java
+++ src/main/ui/gui/JournalUI.java
@@ -18,7 +18,13 @@ import javax.swing.JTextField;
 import model.Entry;
 import model.Journal;
 
+/*
+ * EFFECTS: Represents the graphical user interface for the journal application; 
+ * -------- allows user to input and view journal entries;
+ */
 public class JournalUI extends JFrame implements ActionListener {
+
+    // ==========--FIELDS--==========
 
     private Journal journal;
     private ArrayList<Entry> entries;
@@ -27,7 +33,7 @@ public class JournalUI extends JFrame implements ActionListener {
     private String content;
     private String mood;
 
-    private JLabel dateLabel; 
+    private JLabel dateLabel;
     private JLabel titleLabel;
     private JLabel contentLabel;
     private JLabel moodLabel;
@@ -41,27 +47,51 @@ public class JournalUI extends JFrame implements ActionListener {
     private JButton viewEntriesButton;
     private JButton viewFormButton;
 
-    private JList<String> allTitle;
+    private JList<String> allTitle; // TODO: what does this do again: list of all titles of entries in journal
     private DefaultListModel<String> data;
+
     private JPanel mainPanel;
+    private JPanel logPanel; // TODO: create new logPanelfor each saved log entry, stack in main display
 
-    int textSize;
+    static final int TEXT_SIZE = 50;
+    static final int APP_WIDTH = 600;
+    static final int APP_HEIGHT = 600;
 
-    public JournalUI() { // TODO: write documentation for every method
+    // ==========--CONSTRUCTOR--==========
+
+    /*
+     * EFFECTS: creates a new journal user interface with a window that pops up;
+     */
+    public JournalUI() { // TODO: do I need to create a JFrame field, run setContentPanel() etc.?
         super("Dr. Mouse"); // name of desktop window that pops up
-        ImageIcon img = new ImageIcon("./graphics/icon.png");
-        setIconImage(img.getImage());
-
-        init();
-
-        setSize(530, 500); // try getPreferredSize() // TODO: make fixed window
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setAppIcon();
+        initAll();
+        setLocation(400, 200); // positions window on desktop
+        setSize(APP_WIDTH, APP_HEIGHT); // try getPreferredSize() // TODO: make fixed window by setting max size to this
+                                        // size too
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    private void init() {
+    // ==========--METHODS--==========
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: sets the icon of the application window to the image located in the
+     * -------- graphics folder;
+     */
+    private void setAppIcon() {
+        ImageIcon img = new ImageIcon("./graphics/icon.png");
+        setIconImage(img.getImage());
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: initializes all the components of the journal user interface;
+     */
+    private void initAll() {
         initLabels();
-        initText();
+        initTexts();
 
         addEntryButton = new JButton("Submit Entry");
         viewEntriesButton = new JButton("View Entries");
@@ -85,6 +115,10 @@ public class JournalUI extends JFrame implements ActionListener {
         add(mainPanel);
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: adds all the components to the main panel;
+     */
     private void addAllToMain() {
         mainPanel.add(dateLabel, BorderLayout.PAGE_START);
         // mainPanel.add(dateText);
@@ -104,25 +138,75 @@ public class JournalUI extends JFrame implements ActionListener {
         update();
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: initializes all the labels of the journal user interface;
+     */
     private void initLabels() {
-        dateLabel = new JLabel("Fill in the boxes below and submit to create a new entry (content necessary)." + LocalDate.now().toString());
+        dateLabel = new JLabel("Fill in the boxes below and submit to create a new entry (content necessary)."
+                + LocalDate.now().toString());
         titleLabel = new JLabel("Title");
         contentLabel = new JLabel("Content");
         moodLabel = new JLabel("Mood");
     }
 
-    private void initText() {
-        textSize = 50;
+    /*
+     * MODIFIES: this
+     * EFFECTS: initializes all the text fields of the journal user interface;
+     */
+    private void initTexts() {
         // dateText = new JTextField(textSize-10);
-        titleText = new JTextField(textSize-5);
+        titleText = new JTextField(TEXT_SIZE - 5);
         contentText = new JTextArea("REQUIRED: Click to edit.", 3, 50);
         // contentText.setFont();
         contentText.setWrapStyleWord(true); // makes sure box doesn't shrink when no text present
-        moodText = new JTextField(textSize-10);
+        moodText = new JTextField(TEXT_SIZE - 10);
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: updates the journal user interface based on the event that occurred;
+     */
     @Override
     public void actionPerformed(ActionEvent event) {
+        checkAddEntryButton(event);
+        checkViewEntriesButton(event);
+        checkViewFormButton(event);
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: checks if the viewFormButton was clicked and displays the entry
+     * -------- input form of the journal if it was;
+     */
+    private void checkViewFormButton(ActionEvent event) {
+        if (event.getSource() == viewFormButton) {
+            removeComponents();
+            mainPanel.remove(viewFormButton);
+            addAllToMain();
+            update();
+        }
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: checks if the viewEntriesButton was clicked and displays the
+     * -------- journal's entire entry log if it was;
+     */
+    private void checkViewEntriesButton(ActionEvent event) {
+        if (event.getSource() == viewEntriesButton) {
+            removeComponents();
+            mainPanel.add(viewFormButton);
+            update();
+        }
+    }
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: checks if the add entry button was clicked and adds the entry to the
+     * -------- journal if it was;
+     */
+    private void checkAddEntryButton(ActionEvent event) {
         if (event.getSource() == addEntryButton) {
             title = titleText.getText();
             // date = dateText.getDate();
@@ -136,28 +220,23 @@ public class JournalUI extends JFrame implements ActionListener {
             journal.addEntry(entry);
             clearFields();
         }
-
-        if (event.getSource() == viewEntriesButton) {
-            removeFields();
-            mainPanel.add(viewFormButton);
-            update();
-        }
-
-        if (event.getSource() == viewFormButton) {
-            removeFields();
-            mainPanel.remove(viewFormButton);
-            addAllToMain();
-            update();
-        }
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: updates the journal user interface graphics;
+     */
     private void update() {
         update(getGraphics());
         this.pack();
-        setSize(530, 500);
+        setSize(APP_WIDTH, APP_HEIGHT);
         mainPanel.setVisible(true);
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: clears all the text fields of the journal user interface;
+     */
     private void clearFields() {
         // dateText.setText("");
         titleText.setText("");
@@ -165,7 +244,11 @@ public class JournalUI extends JFrame implements ActionListener {
         moodText.setText("");
     }
 
-    private void removeFields() {
+    /*
+     * MODIFIES: this
+     * EFFECTS: removes all the components of the journal user interface;
+     */
+    private void removeComponents() {
         mainPanel.remove(dateLabel);
         // mainPanel.remove(dateText);
         mainPanel.remove(titleLabel);
